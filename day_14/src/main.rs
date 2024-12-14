855715d8325955332ad7b1f32f267dcf1fd41ba8
JadenB <jadenbbernal@gmail.com>
2024-12-14 13:54:14 -0800

    Day 14 improvements

--- day_14/src/main.rs
+++ day_14/src/main.rs
@@ -1,4 +1,8 @@
-use std::{fs, io::{self, Write}};
+use std::{
+    borrow::Borrow,
+    fs,
+    io::{self, Write},
+};
 
 #[derive(Debug)]
 struct Robot {
@@ -23,32 +27,26 @@ impl Robot {
     }
 }
 
+fn wrapped(v: i32, len: i32) -> i32 {
+    if v >= 0 {
+        v % len
+    } else {
+        len + ((v + 1) % len) - 1
+    }
+}
+
 fn final_pos(r: &Robot, seconds: i32, w: i32, h: i32) -> (i32, i32) {
-            let final_x = r.x + seconds * r.vx;
-            let final_y = r.y + seconds * r.vy;
-
-            let final_x = if final_x >= 0 {
-                final_x % w
-            } else {
-                w + ((final_x + 1) % w) - 1
-            };
-            let final_y = if final_y >= 0 {
-                final_y % h
-            } else {
-                h + ((final_y + 1) % h) - 1
-            };
-
-            (final_x, final_y)
+    (
+        wrapped(r.x + seconds * r.vx, w),
+        wrapped(r.y + seconds * r.vy, h),
+    )
 }
 
-fn final_positions(robots: &[Robot], seconds: i32, w: i32, h: i32) -> Vec<(i32, i32)> {
-    robots
-        .iter()
-        .map(|r| final_pos(r, seconds, w, h))
-        .collect()
-}
-
-fn safety_factor(robot_positions: &[(i32, i32)], w: i32, h: i32) -> i32 {
+fn safety_factor<T, I>(robot_positions: I, w: i32, h: i32) -> i32
+where
+    T: Borrow<(i32, i32)>,
+    I: IntoIterator<Item = T>,
+{
     let mut q1 = 0;
     let mut q2 = 0;
     let mut q3 = 0;
@@ -58,6 +56,7 @@ fn safety_factor(robot_positions: &[(i32, i32)], w: i32, h: i32) -> i32 {
     let mid_y = h / 2;
 
     for pos in robot_positions {
+        let pos = pos.borrow();
         if pos.0 < mid_x && pos.1 < mid_y {
             q1 += 1;
         } else if pos.0 > mid_x && pos.1 < mid_y {
@@ -73,42 +72,34 @@ fn safety_factor(robot_positions: &[(i32, i32)], w: i32, h: i32) -> i32 {
 }
 
 fn print_grid(w: i32, h: i32, robots: &[Robot]) {
-    let mut grid: Vec<Vec<u8>> = (0..h).map(|_| (0..w).map(|_| b'.').collect()).collect();
-
     let mut s = 0;
     loop {
-        print!("{esc}[1;1H", esc = 27 as char);
-        println!("===== {} =====", s);
-        for r in robots {
-            let (x, y) = final_pos(r, s, w, h);
-            grid[y as usize][x as usize] = b'O';
-        }
+        let safety = safety_factor(robots.iter().map(|r| final_pos(r, s, w, h)), w, h);
+        if safety < 50000000 {
+            let mut grid: Vec<Vec<u8>> = (0..h).map(|_| (0..w).map(|_| b'.').collect()).collect();
+            for r in robots {
+                let (x, y) = final_pos(r, s, w, h);
+                grid[y as usize][x as usize] = b'O';
+            }
 
-    let final_positions = final_positions(robots, s, w, h);
-    let safety = safety_factor(&final_positions, w, h);
-        for row in grid.iter_mut() {
-            let _ = io::stdout().write(row);
-            let _ = io::stdout().write(b"\n");
-            row.fill(b'.');
-        }
+            for row in grid.iter_mut() {
+                let _ = io::stdout().write(row);
+                let _ = io::stdout().write(b"\n");
+            }
 
-    if safety < 50019008 {
-        return;
-    }
+            return;
+        }
 
         s += 1;
     }
 }
 
 fn main() {
-    //let (input, w, h) = (fs::read_to_string("input/input_ex.txt").unwrap(), 11, 7);
+    // let (input, w, h) = (fs::read_to_string("input/input_ex.txt").unwrap(), 11, 7);
     let (input, w, h) = (fs::read_to_string("input/input.txt").unwrap(), 101, 103);
-
     let robots: Vec<Robot> = input.lines().map(Robot::from_line).collect();
 
-    let final_positions = final_positions(&robots, 100, w, h);
-    println!("{final_positions:?}");
-    let result = safety_factor(&final_positions, w, h);
+    let result = safety_factor(robots.iter().map(|r| final_pos(r, 100, w, h)), w, h);
     println!("{result}");
 
     print_grid(w, h, &robots);
